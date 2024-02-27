363bc546aae373957494dbcb4aa8e51984a6bd3f
Luna Nordbergh <luna.nordbergh2@gmail.com>
2024-02-27 16:32:07 +0100

    new file:   week 1/main.py

---
+++ week 1/main.py
@@ -0,0 +1,19 @@
+print("Hello World! This is Luna typing python :) 😁👍")
+
+value1 = 1
+value2 = 2
+
+#This is a coment
+
+#This is Addistion (+)
+print("1 + 2 = " + str(1 + 2))
+
+#This is Subtraction (-)
+#use str() to convert to string
+print("2 - 1 = " + str(value2 - value1))
+
+#This is Multiplication (*)
+print("2 * 5 = " + str(2 * 5))
+
+#This is Divition (/)
+print("2 / 2 = " + str(value2 / value2))
