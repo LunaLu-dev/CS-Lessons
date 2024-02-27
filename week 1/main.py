b390247404ed41b1ab5be7aac7ce518ef0c39a3c
Luna Nordbergh <luna.nordbergh2@gmail.com>
2024-02-27 16:45:42 +0100

    added types 	modified:   week 1/main.py

--- week 1/main.py
+++ week 1/main.py
@@ -1,7 +1,9 @@
+print()
 print("Hello World! This is Luna typing python :) 😁👍")
 
 value1 = 1
 value2 = 2
+string1 = "Hello I am a string"
 
 #This is a coment
 
@@ -17,3 +19,14 @@ print("2 * 5 = " + str(2 * 5))
 
 #This is Divition (/)
 print("2 / 2 = " + str(value2 / value2))
+
+#This primts a blank line
+print()
+
+#This is printing the type of the values
+print("Both " + str(2.5) + " and " + str(2.0) + " is " + str(type(2.0)))
+print("This text like this '"+ string1 +"' is a " + str(type(string1)))
+print("And single numbers like " + str(value1) + " is a " + str(type(value1)))
+
+#another blank line for style
+print()
